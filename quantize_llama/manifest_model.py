7aa24b23a32e03037282073c339156e4fdb1cfe5
Albert Tseng <alberttseng@mk-xii-06.cloud.together.ai>
2024-11-20 14:38:10 -0800

    patch

--- quantize_llama/manifest_model.py
+++ quantize_llama/manifest_model.py
@@ -23,24 +23,44 @@ parser.add_argument('--output_path', type=str)
 
 def main(args):
     model, model_str = model_from_hf_path(args.hf_path)
-    manifested_model = AutoModelForCausalLM.from_pretrained(args.hf_path)
+    orig_model = AutoModelForCausalLM.from_pretrained(model_str, torch_dtype='auto')
 
-    for module in model.modules():
-        if isinstance(module, QuantizedLinear):
-            module.mode = 'train-fixW'
-    
     tok = torch.tensor([[1]]).cuda()
     model(tok)
 
-    for name, module in model.named_modules():
+    names = [n for n, _ in model.named_modules()]
+    for name in names:
+        try:
+            module = attrgetter(name)(model)
+        except:
+            continue
         if isinstance(module, QuantizedLinear):
+            module.codebook_class.cache_hatW(
+                module.trellis,
+                module.had_left,
+                module.had_right,
+                module.K_left,
+                module.K_right,
+                len(module.SV),
+                len(module.SU),
+                module.rcp,
+                module.tp_rank,
+            )
             w = module.codebook_class.hatW.float()
-            w = ((w*module.SU).T * module.SV).T * module.codebook_class.scale
-            manifested_linear = attrgetter(name)(manifested_model)
-            manifested_linear.weight.copy_(w.to(manifested_linear.weight.dtype))
-    # set name or path to name of original model for tokenizer loading
-    manifested_model.config._name_or_path = model_str
-    manifested_model.save_pretrained(args.output_path, safe_serialization=True)
+            w = ((w*module.SU).T * module.SV).T * module.codebook_class.scale            
+            orig_linear = attrgetter(name)(orig_model)
+            orig_linear.weight.copy_(w.to(orig_linear.weight.dtype))
+            del module.codebook_class.hatW
+            split_attr = name.split('.')
+            setattr(
+                attrgetter('.'.join(split_attr[:-1]))(model), split_attr[-1],
+                orig_linear)
+            print(name)
+
+    model.config._name_or_path = model_str
+    model.config._quantized_name_or_path = args.hf_path
+    del model.config.quip_params
+    model.save_pretrained(args.output_path, safe_serialization=True)
             
 if __name__ == '__main__':
     torch.set_grad_enabled(False)
